f71cbe2424166a848c01181836a1b3925bcc3f90
sadness_ojisan <sadness.ojisan@gmail.com>
2021-05-07 07:04:54 +0900

    add comonent

--- src/lib/Code.svelte
+++ src/lib/Code.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import A11yDark from "./codes/A11yDark.svelte";
   import A11yLight from "./codes/A11yLight.svelte";
+  import Agate from "./codes/Agate.svelte";
+  import Arta from "./codes/Arta.svelte";
 
   import Dark from "./codes/Dark.svelte";
+  import Default from "./codes/Default.svelte";
   import Github from "./codes/Github.svelte";
   import type { StyleKey } from "./const/component-pair";
 
@@ -18,6 +21,12 @@
     <A11yDark />
   {:else if skin === "a11y-light"}
     <A11yLight />
+  {:else if skin === "default"}
+    <Default />
+  {:else if skin === "agate"}
+    <Agate />
+  {:else if skin === "arta"}
+    <Arta />
   {:else}
     <div>{skin}</div>
   {/if}
